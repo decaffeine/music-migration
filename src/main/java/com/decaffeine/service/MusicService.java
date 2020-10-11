ad5b98d9433e0d06cfd4ba437e24271812d85b59
decaffeine <decaffeine12@gmail.com>
2020-10-12 00:36:32 +0900

    Selenium ChromeDriver Local 작동 확인

--- src/main/java/com/decaffeine/service/MusicService.java
+++ src/main/java/com/decaffeine/service/MusicService.java
@@ -1,11 +1,39 @@
 package com.decaffeine.service;
 
 import org.openqa.selenium.WebDriver;
-import org.springframework.stereotype.Service;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-@Service
+//import org.springframework.stereotype.Service;
+//
+//@Service
 public class MusicService {
 
     private WebDriver webDriver;
+
+    public static void main(String[] args) {
+        MusicService musicService = new MusicService();
+        musicService.crawl();
+    }
+
+    private String base_url;
+
+    public MusicService() {
+        System.setProperty("webdriver.chrome.driver","/Users/hyejin/projects/music-migration/src/resources/chromedriver");
+        webDriver = new ChromeDriver();
+        base_url = "https://music.bugs.co.kr";
+
+    }
+
+    public void crawl() {
+        try {
+            webDriver.get(base_url);
+            System.out.println(webDriver.getPageSource());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            webDriver.close();
+        }
+    }
 
 }
