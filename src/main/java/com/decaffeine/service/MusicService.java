b19895f76d91491c7ff55d574345f7a78861b08f
decaffeine <decaffeine12@gmail.com>
2020-10-16 00:14:33 +0900

    local debugger address 추가

--- src/main/java/com/decaffeine/service/MusicService.java
+++ src/main/java/com/decaffeine/service/MusicService.java
@@ -1,12 +1,17 @@
 package com.decaffeine.service;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.stereotype.Service;
 
-//import org.springframework.stereotype.Service;
-//
-//@Service
+@Service
 public class MusicService {
+
+    private static final String WEB_DRIVER_PATH = "/Users/hyejin/projects/music-migration/src/main/resources/chromedriver";
+    private static final String TARGET_PAGE_PATH = "https://music.bugs.co.kr";
 
     private WebDriver webDriver;
 
@@ -18,15 +23,43 @@ public class MusicService {
     private String base_url;
 
     public MusicService() {
-        System.setProperty("webdriver.chrome.driver","/Users/hyejin/projects/music-migration/src/resources/chromedriver");
-        webDriver = new ChromeDriver();
-        base_url = "https://music.bugs.co.kr";
-
+        System.setProperty("webdriver.chrome.driver", WEB_DRIVER_PATH);
+        ChromeOptions chromeOptions = new ChromeOptions();
+//        chromeOptions.setCapability("ignoreProtectedModeSettings", true);
+        chromeOptions.setExperimentalOption("debuggerAddress","127.0.0.1:9222");
+        webDriver = new ChromeDriver(chromeOptions);
     }
 
     public void crawl() {
         try {
-            webDriver.get(base_url);
+            String orignalWindow = webDriver.getWindowHandle();
+
+            webDriver.get(TARGET_PAGE_PATH);
+            WebElement element = webDriver.findElement(By.className("login"));
+            element.click();
+            WebElement paycoElement = webDriver.findElement(By.className("btnPaycoLogin"));
+            paycoElement.click();
+
+            for (String windowHandle : webDriver.getWindowHandles()) {
+                if (!orignalWindow.contentEquals(windowHandle)) {
+                    webDriver.switchTo().window(windowHandle);
+                    break;
+                }
+            }
+
+            WebElement idField = webDriver.findElement(By.id("id"));
+            idField.sendKeys("twilightblue.k@gmail.com");
+            WebElement pwField = webDriver.findElement(By.id("pw"));
+            pwField.sendKeys("...");
+
+            WebElement loginButton = webDriver.findElement(By.id("loginBtn"));
+            loginButton.submit();
+
+            Thread.sleep(2000);
+
+            webDriver.switchTo().window(orignalWindow);
+
+            webDriver.get("https://music.bugs.co.kr/user/library/like/track?wl_ref=M_left_03_02");
             System.out.println(webDriver.getPageSource());
 
         } catch (Exception e) {
