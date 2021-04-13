74af27e0817a940b878d5ed943101f1e7069c511
decaffeine <decaffeine12@gmail.com>
2021-04-14 00:09:31 +0900

    feat: 애플뮤직 추가 시도중..

--- src/main/java/com/decaffeine/service/MusicService.java
+++ src/main/java/com/decaffeine/service/MusicService.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.stereotype.Service;
 
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,13 +17,13 @@ public class MusicService {
     private static final String TARGET_PAGE_PATH = "https://music.bugs.co.kr";
 
     private WebDriver webDriver;
+    private List<String> titles = new ArrayList<>();
+    private List<String> artists = new ArrayList<>();
 
     public static void main(String[] args) {
         MusicService musicService = new MusicService();
         musicService.crawl();
     }
-
-    private String base_url;
 
     public MusicService() {
         System.setProperty("webdriver.chrome.driver", WEB_DRIVER_PATH);
@@ -53,32 +52,24 @@ public class MusicService {
                 }
             }
 
-            Thread.sleep(2000);
+            Thread.sleep(3000);
 
             WebElement loginButton = webDriver.findElement(By.id("loginBtn"));
             loginButton.submit();
-
             Thread.sleep(2000);
 
             webDriver.switchTo().window(orignalWindow);
 
-            webDriver.get("https://music.bugs.co.kr/user/library/like/track?wl_ref=M_left_03_02");
-            //System.out.println(webDriver.getPageSource());
-            WebElement trackList = webDriver.findElement(By.id("USER_LIKE_TRACK1234"));
-            List<WebElement> titleElements = trackList.findElements(By.className("title"));
-            List<WebElement> artistElements = trackList.findElements(By.className("artist"));
+            searchBugsAndAdd(getPageUrl(1));
 
-            List<String> titles = new ArrayList<>();
-            List<String> artists = new ArrayList<>();
-            for (int i = 1 ; i < titleElements.size() ; i++) {
-                titles.add(titleElements.get(i).getText());
-                artists.add(artistElements.get(i).getText().replaceAll("\\([^()]*\\)",""));
-            }
+            System.out.println("done");
+            System.out.println(titles.size());
 
             for (int i = 0 ; i < titles.size() ; i++) {
-                System.out.println(i + " ## " + titles.get(i) + " " + artists.get(i));
-                webDriver.get("https://music.apple.com/us/search?term=" +  titles.get(i) + " " + artists.get(i));
-                Thread.sleep(10000); // APPLE MUSIC 반응속도가 생각보다 엄청 느림
+                String query = titles.get(i) + " " + artists.get(i);
+                System.out.println(i + " ## " + query);
+
+                searchApplemusic(query);
             }
 
         } catch (Exception e) {
@@ -87,5 +78,33 @@ public class MusicService {
             webDriver.close();
         }
     }
+
+    private void searchBugsAndAdd(String url) {
+        webDriver.get(url);
+        WebElement trackList = webDriver.findElement(By.id("USER_LIKE_TRACK1234"));
+        List<WebElement> titleElements = trackList.findElements(By.className("title"));
+        List<WebElement> artistElements = trackList.findElements(By.className("artist"));
+
+        for (int i = 1 ; i < titleElements.size() ; i++) {
+            titles.add(titleElements.get(i).getText());
+            artists.add(artistElements.get(i).getText().replaceAll("\\([^()]*\\)",""));
+            System.out.println(titleElements.get(i));
+        }
+
+    }
+
+    private String getPageUrl(int i) {
+        if (i == 1) {
+            return "https://music.bugs.co.kr/user/library/like/track?wl_ref=M_left_03_02";
+        }
+        return "https://music.bugs.co.kr/user/library/like/track?sort=REGDATE&page=" + i + "&highQualityOnly=N&radioOnly=N";
+    }
+
+    private void searchApplemusic(String query) throws InterruptedException {
+        webDriver.get("https://music.apple.com/us/search?term=" +  query);
+        Thread.sleep(20000); // APPLE MUSIC 반응속도가 생각보다 엄청 느림
+
+    }
+
 
 }
